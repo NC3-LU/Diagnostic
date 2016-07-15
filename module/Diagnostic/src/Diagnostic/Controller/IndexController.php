f576cda72e3a55e2f67ee5507b0ac8c3688df67c
Jerome De Almeida <dealmeida.jerome@gmail.com>
2016-07-15 09:34:10 +0200

    new model for delivery

--- module/Diagnostic/src/Diagnostic/Controller/IndexController.php
+++ module/Diagnostic/src/Diagnostic/Controller/IndexController.php
@@ -814,7 +814,7 @@ class IndexController extends AbstractActionController
                     $calculService = $this->getServiceLocator()->get('Diagnostic\Service\CalculService');
                     $calculResults = $calculService->calcul();
 
-                    $word = new TemplateProcessorService('data/resources/modele_v0.8.docx');
+                    $word = new TemplateProcessorService('data/resources/modele_v0.9.docx');
                     $word->generateWord($data, $questions, $calculResults['recommandations'], $information,  $translator);
                 }
             }
