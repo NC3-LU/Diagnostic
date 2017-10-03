eac21c61c9245b14d2f355c784f03666de7da18f
BenjaminJoly <benjamin.joly@securitymadein.lu>
2017-10-03 10:26:47 +0200

    Correction for the changePassword script

--- scripts/changePassword.sh
+++ scripts/changePassword.sh
@@ -4,57 +4,94 @@
 
 sqlPassword=diagnosticpass
 user="${1:-}"
+strongPass=0
+password="something"
+password_repeat="else"
 
-echo -e "\033[93m###############################################################################"
-echo -e "\033[93m#                             Password Changer                                #"
-echo -e "\033[93m###############################################################################"
+if [ $# -ne 1]; then
+	echo "You need to specify the user of the diagnostic which password should be used. \nExample : ./changePassword.sh \"diagnostic@cases.lu\""
+	exit 0
+fi
+	
+echo -e "\033[93m###############################################################################\033[0m"
+echo -e "\033[93m#                             Password Changer                                #\033[0m"
+echo -e "\033[93m###############################################################################\033[0m"
 
-echo -e "\033[94m Connecting to database..."
-mysql -u diagnostic -p"${sqlPassword}" -e "USE diagnostic;"
-if [ "$?" -eq 0 ]; then
-    echo -e "\033[94m Connection established"
-
-    echo -e "\033[94m"
-    read -s -p "Please, set a new password for ${user} > " password
-    echo -e "\033[0m"
-
-    passwordLength=${#password}
-
-    # Testing the password
-    if [[ $password =~ .*\d+.* ]]; then
-        echo "We got a digit"
-    else
-        echo "No digit in $password"
-    fi
-
-
-    echo -e "\033[94m"
-    read -s -p "Repeat password > " password_repeat
-    echo -e "\033[0m"
-
-    while [ $password != $password_repeat ]
-    do
-    echo -e "\033[94mThe passwords do not match. Try again please"
-    echo -e "\033[94m"
-    read -s -p "Enter ${user} password > " password
-    echo -e "\033[0m"
-
-    echo -e "\033[94m"
-    read -s -p "Repeat password > " password_repeat
-    echo -e "\033[0m"
-    done
-
-
-    hash_password=$(php -r 'echo password_hash(${password}, PASSWORD_DEFAULT);')
-    echo "The password hash is : ${hash_password}"
-    # mysql -h localhost -u diagnostic -p"${sqlPassword}" --database=diagnostic -e "UPDATE users SET password='${hash_password}' WHERE email='${user}';"
-else
-    echo "Mysql connection failed"
+#Connexion test to the database
+echo -e "\033[94mConnecting to database and verify user...\033[0m"
+mysql -u diagnostic -p"${sqlPassword}" -e "USE diagnostic;" > /dev/null 2>&1
+if [ "$?" -ne 0 ]; then
+	echo -e "\033[31mConnection to the diagnostic database has failed.\033[0m\n\033[32mDoes this script have the right sql password for the diagnostic user ?\033[0m"
+	exit 0
 fi
 
+#Testing if the user exists
+user_exists=$(mysql -ss -N -u diagnostic -p"${sqlPassword}" -e "SELECT id FROM diagnostic.users WHERE email='$user';")
+if [ ! -z $user_exists ]; then
+    echo -e "\033[94m Connection established, user exists\033[0m"
+	#Check strength and repetition of the password
+	while [ "$password" != "$password_repeat" ] || [ $strongPass -eq 0 ]; do
+		strongPass=1
+		password=''
+		echo -e -n "\033[94mPlease, set a new password for ${user} > \033[0m"
+		read -s password
+		passwordLength=${#password}
 
+		# Testing the password
+		if ! [[ $password =~ .*[0-9]+.* ]]
+		then
+			strongPass=0
+			echo "\033[31mPassword need to contain at least one digit to be a valid password\033[0m"
+		fi
+	
+		if ! [[ $password =~ .*[A-Z]+.* ]]
+		then
+			strongPass=0
+			echo "\033[31mPassword need to contain at least one upper char to be a valid password\033[0m"
+		fi
+		
+		if ! [[ $password =~ .*[a-z]+.* ]]
+		then
+			strongPass=0
+			echo "\033[31mPassword need to contain at least one lower char to be a valid password\033[0m"
+		fi
+		
+		if ! [ $passwordLength -gt 7 ]
+		then
+			strongPass=0
+			echo "\033[31mPassword need to contain at least 8 chars to be a valid password\033[0m"
+		fi
+		
+		if ! [[ $password =~ .*[^[:alnum:]]+.* ]]
+		then
+			strongPass=0
+			echo "\033[31mPassword need to contain at least one special char to be a valid password\033[0m"
+		fi
 
+		#ask to repeat password if the password is strong enough
+		if [[ $strongPass -eq 1 ]]; then
+		
+			echo -e "\033[94mRepeat password > \033[0m"
+			read -s password_repeat
+			if [[ "$password" != "$password_repeat" ]]; then
+				echo -e "\033[94mThe passwords do not match. Try again please\033[0m"
+			fi
+		fi
+	done
 
-echo -e "\033[93m###############################################################################"
-echo -e "\033[93m#                            Password Changer done                            #"
-echo -e "\033[93m###############################################################################"
+	#hashing the password
+	hash_password=`/usr/bin/php <<EOF
+	<?php echo password_hash(("$password"), PASSWORD_DEFAULT); ?>
+EOF`
+
+	#Send the new password to the database
+	printf -v hash_password $hash_password
+    mysql -h localhost -u diagnostic -p"${sqlPassword}" -e "UPDATE diagnostic.users SET password='$hash_password' WHERE email='$user';" > /dev/null 2>&1
+else
+    echo "\033[35mMysql connection failed. Do this script have the right sql password ?\033[0m"
+	exit 0
+fi
+
+echo -e "\033[93m###############################################################################\033[0m"
+echo -e "\033[93m#                            Password Changer done                            #\033[0m"
+echo -e "\033[93m###############################################################################\033[0m"
